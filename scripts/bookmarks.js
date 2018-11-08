448d8cae6cfb976e8ef49587df59ef35d1362af8
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-08 12:03:03 -0800

    first pass at generating item elements and rendering

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -1,31 +1,62 @@
 'use strict';
 /*eslint-env jquery*/
-/* global store, API $ */
+/* global STORE, API $ */
 
-$.fn.extend({
-  serializeJson: function(){
-    const newObject = {};
-    const data = new FormData(this[0]);
-    data.forEach((value, key) => {
-      newObject[key] = value;
-    });
-    return JSON.stringify(newObject);
+const Bookmark = (function (){
+
+  function generateHTMLString (bookmarks){
+    return STORE.bookmarks.map(bookmark => generateItemElement(bookmark)).join('');
   }
-});
+
+  function generateItemElement (newObject){
+    return `
+    <ul class="bookmark-item">
+    <li class='bookmark-item-element' data-item-id='${newObject.id}'>
+      <div>${newObject.name}</div>
+      <div>${newObject.rating}</div>
+      <div>${newObject.url}</div>
+      <div>${newObject.description}</div>
+    </li>
+    `;
+  }
+
+  function render (){
+    $('ul').html(generateHTMLString(STORE.bookmarks));
+  }
 
 
-
-function newBookmarkSubmit(){
-  $('#new-item').on('submit', function(event){
-    event.preventDefault();
-    console.log('new item handler worked');
-    const result = $(event.target).serializeJson();
-    API.createBookmark(result, (newBookmark) => {
-      store.createItemInStore(newBookmark);
-      render();
-    });
-    console.log(result);
+  $.fn.extend({
+    serializeJson: function(){
+      const newObject = {};
+      const data = new FormData(this[0]);
+      data.forEach((value, key) => {
+        newObject[key] = value;
+      });
+      return JSON.stringify(newObject);
+    }
   });
-}
 
-$(newBookmarkSubmit);
+
+
+  function newBookmarkSubmit(){
+    $('#new-item').on('submit', function(event){
+      event.preventDefault();
+      console.log('new item handler worked');
+      const result = $(event.target).serializeJson();
+      API.createBookmark(result, (newBookmark) => {
+        store.createItemInStore(newBookmark);
+        render();
+      });
+      console.log(result);
+    });
+  }
+
+  $(newBookmarkSubmit);
+
+
+  return {
+    render
+  };
+
+
+} ());
